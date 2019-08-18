cdf04587adfaeb671be7e12485a1986a06634d6d
Hannes Oldenburg <hannes.christian.oldenburg@gmail.com>
2019-08-18 20:43:04 +0000

    persist state

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 window.onload = function(){
+    var currentState = localStorage.getItem('state') || 'R';
     var el = document.getElementById("state");
+    el.innerText = currentState;
     document.onclick = function(ev){
         if(el.innerText === "R"){
             el.innerText = "L";
+            localStorage.setItem("state", "L")
         } else {
             el.innerText = "R";
+            localStorage.setItem("state", "R")
         }
     }
 }
