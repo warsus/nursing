ff91f170e4bb9177a51aa57a6cd0ab6453ed73c5
Hannes Oldenburg <hannes.christian.oldenburg@gmail.com>
2019-08-19 19:40:17 +0000

    prompt for homescreen

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ window.onload = function(){
         }
     }
 }
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  showInstallPromotion();
+});
