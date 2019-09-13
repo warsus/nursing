04fdfb48e7493cac90319ef20014b76c6417b73e
Hannes Oldenburg <hannes.christian.oldenburg@gmail.com>
2019-09-13 19:12:40 +0000

    test visualization

--- index.js
+++ index.js
@@ -1,15 +1,60 @@
+
+
 window.onload = function(){
-    var currentState = localStorage.getItem('state') || 'R';
-    var el = document.getElementById("state");
-    el.innerText = currentState;
-    document.onclick = function(ev){
-        if(el.innerText === "R"){
+    function renderState(){
+        var state = localStorage.getItem("state") || 'R';
+        if(state === "L"){
+            img.setAttribute("transform", "");
             el.innerText = "L";
-            localStorage.setItem("state", "L")
-        } else {
+            img.className.baseVal = "";
+            //localStorage.setItem("state", "L")
+        } else if (state === "R") {
+            img.setAttribute("transform", "translate(400,0) scale(-1,1)");
+            img.className.baseVal = "";
             el.innerText = "R";
-            localStorage.setItem("state", "R")
+            //localStorage.setItem("state", "R")
+        } else {
+            img.className.baseVal = "hide"
         }
+    }
+    function toggleState(){
+        var state = localStorage.getItem("state")
+        if(state==="R"){
+            localStorage.setItem("state","L");
+        } else {
+            localStorage.setItem("state", "R");
+        }
+    }
+
+    var defaultState = {"side": "R", color: ""}
+    var currentState = localStorage.getItem('state');
+    if(!currentState){
+        localStorage.setItem('state',"R");
+    }
+
+    var img = document.getElementById("baby")
+    var el = document.getElementById("state");
+    var color = document.getElementById("colors")
+   
+    var settings = document.getElementById("settings");
+    var settingsWrapper = document.getElementById("settingsWrapper");
+
+    renderState()
+    //var switchButton = document.getElementById("switch");
+    //settingsWrapper.onclick = function(ev){
+    //    settings.style = "";
+    //}
+    //
+    //
+    console.log(settingsWrapper)
+    switchButton = document.getElementById("switch")
+    color.onclick = function(ev){
+        var target = ev.currentTarget;
+    }
+
+    switchButton.onclick = function(ev){
+        toggleState();
+        renderState();
     }
 }
 
